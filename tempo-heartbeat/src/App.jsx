5e77fb42df92de152587bd80c649783ee86db91a
Kelly <tianqingkellykou@gmail.com>
2024-03-09 20:00:10 -0500

    connect fitbit api, display most recent heartrate

--- tempo-heartbeat/src/App.jsx
+++ tempo-heartbeat/src/App.jsx
@@ -3,16 +3,40 @@ import './App.css'; // Include your CSS file for styling
 
 function App() {
   const [step, setStep] = useState(1);
+  const [heartRateTime, setHeartRateTime] = useState(null)
   const [heartRate, setHeartRate] = useState(null);
   const [targetHeartRate, setTargetHeartRate] = useState('');
   const [selectedSong, setSelectedSong] = useState(null);
 
+  const fetchHeartRateData = async () => {
+    try {
+      const response = await fetch(
+        'https://api.fitbit.com/1/user/-/activities/heart/date/today/today/1min.json',
+        {
+          headers: {
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyM1JTQ0ciLCJzdWIiOiI2TDZYV1YiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJlY2cgcnNldCByb3h5IHJudXQgcnBybyByc2xlIHJjZiByYWN0IHJsb2MgcnJlcyByd2VpIHJociBydGVtIiwiZXhwIjoxNzEwMDUzNDM1LCJpYXQiOjE3MTAwMjQ2MzV9.Vv4jvGSoBHz6fsPR_WhlOXWO6_YnLd8gA0XVTrQ0TW4'
+          }
+        }
+      );
+      if (!response.ok) {
+        throw new Error('Failed to fetch heart rate data');
+      }
+      const data = await response.json();
+      // Extract heart rate from data
+      const mostRecentHeartRate = data["activities-heart-intraday"].dataset.slice(-1)[0];
+
+      // console.log(mostRecentHeartRate)
+
+      setHeartRate(mostRecentHeartRate.value);
+      setHeartRateTime(mostRecentHeartRate.time)
+      setStep(step + 1);
+    } catch (error) {
+      console.error('Error fetching heart rate data:', error);
+    }
+  };
+
   const handleSubmitInput = (e) => {
     e.preventDefault();
-    // Here you can implement logic to get heart rate data (e.g., from Fitbit)
-    // For now, let's simulate heart rate data
-    const simulatedHeartRate = Math.floor(Math.random() * (150 - 60 + 1)) + 60; // Random number between 60 and 150
-    setHeartRate(simulatedHeartRate);
     setStep(step + 1);
   };
 
@@ -45,19 +69,26 @@ function App() {
             <h2>User Input Prompt</h2>
             <form onSubmit={handleSubmitInput}>
               <p>Put on your Fitbit</p>
-              <button type="submit">Next</button>
+              <button type="submit" onClick={fetchHeartRateData}>Next</button>
             </form>
             {heartRate && (
               <div className="heart-rate-display">
                 <div className="heart-animation"></div>
-                <p>Your heart rate: <strong>{heartRate}</strong></p>
+                <p>Your heart rate: <strong>{heartRate}</strong> at <strong>{heartRateTime}</strong></p>
               </div>
             )}
           </div>
         )}
+        
         {step === 2 && (
           <div>
             <h2>Target Heart Rate</h2>
+            {heartRate && (
+              <div className="heart-rate-display">
+                <div className="heart-animation"></div>
+                <p>Your heart rate: <strong>{heartRate}</strong> at <strong>{heartRateTime}</strong></p>
+              </div>
+            )}
             <form onSubmit={handleSubmitTargetHeartRate}>
               <label>
                 Enter your target heart rate:
